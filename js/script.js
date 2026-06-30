9e83d767adcc412b03980b7791c3f6f45a5aa7ba
Irene Cantero <ireterecantero@gmail.com>
2026-06-30 16:31:30 +0200

    nueva direccion

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 
-  // Crear el elemento del cursor personalizado
-  const cursor = document.createElement('div');
-  cursor.classList.add('custom-cursor');
-  document.body.appendChild(cursor);
+  // // Crear el elemento del cursor personalizado
+  // const cursor = document.createElement('div');
+  // cursor.classList.add('custom-cursor');
+  // document.body.appendChild(cursor);
 
-  // Actualizar la posición del cursor personalizado
-  document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-  });
+  // // Actualizar la posición del cursor personalizado
+  // document.addEventListener('mousemove', (e) => {
+  //   cursor.style.left = e.clientX + 'px';
+  //   cursor.style.top = e.clientY + 'px';
+  // });
 
-  // Obtener todos los elementos de proyecto
-  const projects = document.querySelectorAll('.project');
+  // // Obtener todos los elementos de proyecto
+  // const projects = document.querySelectorAll('.project');
 
-  projects.forEach(project => {
-    project.addEventListener('mouseenter', () => {
-      const title = project.getAttribute('data-title');
-      cursor.textContent = title;
-      cursor.style.display = 'block';
-    });
+  // projects.forEach(project => {
+  //   project.addEventListener('mouseenter', () => {
+  //     const title = project.getAttribute('data-title');
+  //     cursor.textContent = title;
+  //     cursor.style.display = 'block';
+  //   });
 
-    project.addEventListener('mouseleave', () => {
-      cursor.style.display = 'none';
-    });
-  });
+  //   project.addEventListener('mouseleave', () => {
+  //     cursor.style.display = 'none';
+  //   });
+  // });
 
-  document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.gallery');
-  new Masonry(grid, {
-    itemSelector: 'img',
-    columnWidth: 'img',
-    gutter: 10
-  });
-});
+//   document.addEventListener('DOMContentLoaded', () => {
+//   const grid = document.querySelector('.gallery');
+//   new Masonry(grid, {
+//     itemSelector: 'img',
+//     columnWidth: 'img',
+//     gutter: 10
+//   });
+// });
